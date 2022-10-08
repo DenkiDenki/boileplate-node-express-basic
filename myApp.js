a0b1de38d1333ab7f6b8dde3bac2d4cb8efaf3f8
Denisa <denisa.gerez@gmail.com>
2022-10-08 15:49:30 -0300

    agregar variables de entorno a node

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
 let express = require("express");
 let app = express();
+require("dotenv").config();
+const SECRET = process.env.MESSAGE_STYLE;
 console.log("Hello World");
 
 app.get("/", (req, res) => {
@@ -8,10 +10,13 @@ app.get("/", (req, res) => {
 
 //middleware to static files
 app.use("/public", express.static(__dirname + "/public"));
-
 //api endpoint json
 app.get("/json", (req, res) => {
-  res.json({ message: "Hello json" });
+  if (SECRET == "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
+  }
 });
 
 module.exports = app;
