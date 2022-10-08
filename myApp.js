d9f469453634634faedb5010a748c8b4750d14f9
Denisa <denisa.gerez@gmail.com>
2022-10-08 11:36:37 -0300

    agregar un endpoint para servir un json en la ruta /json

--- myApp.js
+++ myApp.js
@@ -6,6 +6,12 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+//middleware to static files
 app.use("/public", express.static(__dirname + "/public"));
+
+//api endpoint json
+app.get("/json", (req, res) => {
+  res.json({ message: "Hello json" });
+});
 
 module.exports = app;
